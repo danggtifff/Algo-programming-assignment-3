1195f75841a848aff17b947685673768581820a2
Tiffany Dang <tiffanymdang@gmail.com>
2026-04-06 14:55:58 -0400

    recurrence equation and parsing

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -16,4 +16,43 @@ Case 2: A[i] ≠ B[j] => the characters don't match.
 Either skip A[i] → look at dp[i-1][j]
 Or skip B[j] → look at dp[i][j-1]
 Take the better one.
+
+Recurrence eq:
+
+dp [i][j] = {
+    dp [i-1][j-1] + v(A[i])           if A[i] = B[j]
+    max(dp [i-1][j], dp [i][j-1])     otherwise
+}
 """
+
+"""
+Sources:
+https://medium.com/@prajun_t/dynamic-programming-longest-common-subsequence-lcs-7884b0540638
+https://ics.uci.edu/~goodrich/teach/cs260P/notes/LCS.pdf
+https://www.khoury.northeastern.edu/home/vip/teach/Algorithms/6_dyn_prog/notes/LCS/LCS.pdf
+"""
+
+import sys
+
+def solve(input_text: str) -> tuple[int, str]:
+    # parse the input text
+    lines = input_text.strip().split("/n")
+    idx = 0
+    
+    K = int(lines[idx].strip())
+    idx += 1
+
+    char_value = {}
+    for i in range(K):
+        parts = lines[idx].strip().split()
+        char = parts[0]
+        val = int(parts[1])
+        char_value[char] = val
+        idx += 1
+ 
+    A = lines[idx].strip()
+    idx += 1
+    B = lines[idx].strip()
+ 
+    m = len(A)
+    n = len(B)
